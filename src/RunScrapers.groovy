bea81ac0f6d9fac0445bedce7befee35a72b9084
danielperecz <daniel.perecz2@mail.dcu.ie>
2019-07-27 05:38:52 +0100

    Writing the main code

--- src/RunScrapers.groovy
+++ src/RunScrapers.groovy
@@ -1,5 +1,32 @@
+import groovy.io.FileType
+
 String userName = System.getProperty("user.name")
+String scrapersPath = "/home/" + userName + "/PycharmProjects/Datas_V2/src/scrapers_v2/scrapers"
+int scrapersPathLength = scrapersPath.length()+1
 
-String bashScript = "/home/" + userName + "/PycharmProjects/Datas_V2/src/scrapers_v2/run_mains.sh"
+def allFiles = []
+def dir = new File(scrapersPath)
+dir.eachFileRecurse(FileType.FILES) { file ->
+    allFiles << file
+}
 
-bashScript.execute()
+// Recursively iterate through all nested files & directories
+allFiles.each {
+    String path = it
+
+    // This is the path split up, example: [alic, alic.py]
+    String[] files =  path[scrapersPathLength..-1].split("/")
+
+    // If files is a directory
+    if (files.length > 1 & files[0] != "__pycache__") {
+        // Loop through files
+        files.each {
+            String file = it
+            // If file is not a python test file, run it
+            if (!file.toLowerCase().contains("test") & file.toLowerCase().contains(".py") & !file.toLowerCase().contains("__init__.py")) {
+                //file.execute()
+                println file
+            }
+        }
+    }
+}
